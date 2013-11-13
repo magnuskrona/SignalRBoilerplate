750627a188a42ca075228aa705d5dcdc3874bc48
Magnus Krona <magnus.krona@gmail.com>
2013-11-13 22:08:46 +0100

    Initial commit

---
+++ SignalRBoilerplate/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRBoilerplate.Startup))]
+namespace SignalRBoilerplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            ConfigureAuth(app);
+        }
+    }
+}
